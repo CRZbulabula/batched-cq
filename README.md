bcd74f9a1b1f3cf100569dce088dc8d829727e61
YongzaoDan <532741407@qq.com>
2025-01-16 22:50:15 +0800

    A simple version

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
-# cq
+# Batched CQ
+Consider the scenario where the prover seeks to demonstrate possession of $k$ polynomials $f_1(X),f_2(X),\cdots,f_k(X)$, simultaneously, without actually revealing them. The Cached Quotients (CQ) protocol, as described in this [paper](https://eprint.iacr.org/2022/1763.pdf), can be adapted to efficiently fulfill this requirement.
 
-Implementation of [cached quotients](https://eprint.iacr.org/2022/1763.pdf)
+## Preliminaries
+The following high-quality blogs are recommended for reading before delving into the CQ protocol:
 
-Use `cargo test --release` to run tests.
++ [A (Relatively Easy To Understand) Primer on Elliptic Curve Cryptography](https://blog.cloudflare.com/a-relatively-easy-to-understand-primer-on-elliptic-curve-cryptography/)
++ [Exploring Elliptic Curve Pairings](https://vitalik.eth.limo/general/2017/01/14/exploring_ecp.html)
++ [KZG polynomial commitments](https://dankradfeist.de/ethereum/2020/06/16/kate-polynomial-commitments.html)
 
-See usage example in [src/lib.rs](src/lib.rs) - `test_roundtrip`.
+## Intuition
+To effectively leverage the CQ protocol, we can first explore a method to combine these $k$ polynomials into a single polynomial. Intuitively, multiplying these polynomials presents a natural approach, as it seamlessly incorporates the essential log-derivative method (Lemma 2.3).
+
+Given the n-sparsity of the polynomials $f_1(X),f_2(X),\cdots,f_k(X)$, interpolating the integrated polynomial requires $O(kn)$ Lagrange basis. Consequently, in this batched version of the CQ protocol, the complexity of the prover's work increases to $O(kn \log kn)$, while the complexities of preprocessing, proof size, and verifier's work remain unchanged.
+
+## Correctness
+Let $f_i(X)=\prod_{j=1}^n(x-f_{i,j})$ and $F(X)=\prod_{i=1}^kf_i(X)$. We will show that, following the approach outlined in Lemma 2.3, verifying the correctness of $F(x)$ is equivalent to verifying the correctness of each $f_i(X)$ individually. To begin, we have:
+$$
+\begin{aligned}
+\frac{d}{dx}\log(F(x))&=\frac{d}{dx}\log(\prod_{i=1}^kf_i(X)) \\
+&=\frac{d}{dx}\sum_{i=1}^k\log(f_i(X)) \\
+&=\sum_{i=1}^{k}\sum_{j=1}^n\frac{d}{dx}\log(x-f_{i,j}) \\
+&=\sum_{i=1}^{k}\sum_{j=1}^n\frac{1}{x-f_{i,j}}
+\end{aligned}
+$$
+
+Given the public $T(X)=\prod_{j=1}^N(x-t_j)$, each $f_i(X)$ can be expressed as $f_i(X)=\prod_{j=1}^N(x-t_j)^{m_{i,j}}$. Consequently, we have:
+$$
+\begin{aligned}
+\frac{d}{dx}\log(F(x))&=\frac{d}{dx}\log(\prod_{i=1}^kf_i(X)) \\
+&=\sum_{i=1}^k\sum_{j=1}^Nm_{i,j}\frac{d}{dx}\log((x-t_j)) \\
+&=\sum_{i=1}^k\sum_{j=1}^N\frac{m_{i,j}}{x-t_j} \\
+&=\sum_{j=1}^N\frac{\sum_{i=1}^km_{i,j}}{x-t_j}
+\end{aligned}
+$$
+
+Since the above equalities preserve the same structure as those in Lemma 2.3, we can conclude that the correctness of $F(x)$ is equivalent to the correctness of each $f_i(X)$.
+
+## Quick Benchmark
+We verify the correctness of both $F(X)$ and $f_i(X)$ in the example provided in [src/lib.rs](src/lib.rs) - `test_bach_roundtrip`.
--- src/lib.rs
+++ src/lib.rs
@@ -89,4 +89,88 @@ mod roundtrip_test {
         let res = Verifier::<Bn254, FS>::verify(&vk, &common, &statement, &proof);
         assert!(res.is_ok());
     }
+
+    type ConstantResult<E> = (
+        Table<<E as PairingEngine>::Fr>,
+        Vec<<E as PairingEngine>::Fr>,
+        Index<E>,
+        CommonPreprocessedInput<E>,
+        ProvingKey<E>,
+        Vec<<E as PairingEngine>::G2Affine>,
+    );
+
+    type WitnessResult<E> = (
+        Statement<E>,
+        VerifierKey<E>,
+        Witness<<E as PairingEngine>::Fr>,
+    );
+
+    fn prepare_constant<E: PairingEngine, R: RngCore>(
+        n: usize,
+        rng: &mut R,
+    ) -> ConstantResult<E> {
+        let (srs_g1, srs_g2) = unsafe_setup_from_rng::<E, R>(n - 1, n, rng);
+        let pk = ProvingKey::<E> { srs_g1 };
+        let table_values: Vec<_> = (0..n).map(|_| E::Fr::rand(rng)).collect();
+        let table = Table::new(&table_values).unwrap();
+        let index = Index::<E>::gen(&pk.srs_g1, &srs_g2, &table);
+        let common = Index::<E>::compute_common(&srs_g2, &table);
+        (table, table_values, index, common, pk, srs_g2)
+    }
+
+    fn prepare_witness<E: PairingEngine>(
+        subvector_indices: &[usize],
+        pk: &ProvingKey<E>,
+        srs_g2: &Vec<<E as PairingEngine>::G2Affine>,
+        table: &Table<<E as PairingEngine>::Fr>,
+        table_values: &Vec<<E as PairingEngine>::Fr>,
+    ) -> WitnessResult<E> {
+        let witness_values: Vec<_> = subvector_indices.iter().map(|&i| table_values[i]).collect();
+        let witness = Witness::<E::Fr>::new(&witness_values).unwrap();
+        let statement = Statement::<E> {
+            f: Kzg::<E>::commit_g1(&pk.srs_g1, &witness.f).into(),
+        };
+        let vk = VerifierKey::<E>::new(&srs_g2, table.size, witness.size);
+        (statement, vk, witness)
+    }
+
+    #[test]
+    fn test_bach_roundtrip() {
+        let two: usize = 2;
+        let batch: usize = 4;
+        let n = two.pow(10);
+
+        let mut rng = test_rng();
+        let witness_size = two.pow(3);
+        let mut subvectors_indices: Vec<Vec<usize>> = Vec::new();
+        for _ in 0..batch {
+            subvectors_indices.push(
+                (0..witness_size)
+                    .map(|_| rng.gen_range(0..n - 1))
+                    .collect()
+            );
+        }
+
+        let (table, table_values, index, common, pk, srs_g2) =
+            prepare_constant::<Bn254, StdRng>(n, &mut rng);
+        // Separately verify polynomials
+        for i in 0..batch {
+            let (statement, vk, witness) =
+                prepare_witness::<Bn254>(&subvectors_indices[i], &pk, &srs_g2, &table, &table_values);
+            let proof = Prover::<Bn254, FS>::prove(&pk, &index, &table, &witness, &statement).unwrap();
+            let res = Verifier::<Bn254, FS>::verify(&vk, &common, &statement, &proof);
+            assert!(res.is_ok());
+        }
+
+        // Batched verify polynomials
+        let mut subvector_indices: Vec<usize> = Vec::new();
+        for i in 0..batch {
+            subvector_indices.extend(subvectors_indices[i].iter());
+        }
+        let (statement, vk, witness) =
+            prepare_witness::<Bn254>(&subvector_indices, &pk, &srs_g2, &table, &table_values);
+        let proof = Prover::<Bn254, FS>::prove(&pk, &index, &table, &witness, &statement).unwrap();
+        let res = Verifier::<Bn254, FS>::verify(&vk, &common, &statement, &proof);
+        assert!(res.is_ok());
+    }
 }
